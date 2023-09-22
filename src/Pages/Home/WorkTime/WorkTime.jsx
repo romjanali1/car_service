dceb17c5bae5d7e6c0f9209106bbfd561f58dc99
romjanali1 <mdromgana@gmail.com>
2023-09-22 21:45:57 +0600

    work time edit

--- src/Pages/Home/WorkTime/WorkTime.jsx
+++ src/Pages/Home/WorkTime/WorkTime.jsx
@@ -1,25 +1,32 @@
+import { FcOvertime, FcCallback, FcGlobe } from "react-icons/fc";
 const WorkTime = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 justify-center bg-black text-white py-8 px-8 my-8">
-      <div className="stat">
-        <div>We are open monday-friday</div>
-        <div>24/7</div>
-      </div>
-
-      <div className="stat">
-        <div>Have a question?</div>
-        <div>+8801747001296</div>
-      </div>
-
-      <div className="stat">
-        <div className="stat-figure">
-          <div className="avatar online">
-          </div>
+    <div className="grid grid-cols-1 lg:grid-cols-3 rounded-md justify-center bg-black text-white py-8 px-8 my-8">
+      <div className="flex items-center">
+      <div className="pr-5">
+          <h1><FcOvertime size={40}/></h1>
         </div>
-        <div>Need a website? my linkedin or facebook</div>
-        <div className="flex space-x-4">
-          <a href="https://www.linkedin.com/in/romjan-ali-b3b11627a/">Linkedin</a>
-          <a href="https://www.facebook.com/mnhRomjanAli">Facebook</a>
+        <div>
+        <p>We are open monday-friday</p>
+        <p>24/7</p>
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <div className="pr-5"><h1><FcCallback size={40}/></h1></div>
+        <div>
+        <p>Have a question?</p>
+        <p>+8801747001296</p>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <div className="pr-5"><h1><FcGlobe size={40}/></h1></div>
+        <div>
+        <p>Need a website? my linkedin or facebook</p>
+        <div className="flex space-x-4 text-orange-500">
+          <a href="https://www.linkedin.com/in/romjan-ali-b3b11627a/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+          <a href="https://www.facebook.com/mnhRomjanAli" target="_blank" rel="noopener noreferrer">Facebook</a>
+        </div>
         </div>
       </div>
     </div>
